e6ce733e54d23cc9333b137eb70136f857d1b250
f47h3r <coollink.tnp@gmail.com>
2015-10-29 02:30:13 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fsociety.ninja
+-----
+![fsociety.ninja](https://raw.githubusercontent.com/f47h3r/fsociety.ninja/master/static/img/fsociety.jpg "http://fsociety.ninja")
---
+++ app.py
@@ -0,0 +1,42 @@
+#/usr/bin/env python
+##
+#http://fsociety.ninja
+
+from flask import Flask, render_template, make_response, request
+from flask_sslify import SSLify
+
+import os
+import ssl
+
+app = Flask(__name__, template_folder=os.getcwd())
+sslify = SSLify(app)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.route('/')
+def index_handler():
+    domain = 'fsociety.ninja'
+    resp = make_response(render_template(domain+'.html')) 
+    resp.headers['Server'] = 'fsociety00.dat'
+    return resp
+
+@app.route("/data")
+def data_handler():
+    resp = make_response(render_template('data.html')) 
+    resp.headers['Server'] = 'fsociety00.dat'
+    return resp
+
+if __name__ == '__main__':
+
+    #SSL Configuration
+    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    context.load_cert_chain(certfile="certs/current/validchain.pem", keyfile="certs/current/privkey.pem")
+
+    ##Run SSL
+    app.run(host = '0.0.0.0', port = 3000, ssl_context=context)
+
+    ##Run App
+    #app.run(host = '0.0.0.0', port = 3000)
+    
